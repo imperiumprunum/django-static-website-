7e2f079053c64d9a404a826c88781900fc52146c
Imperium Prunum <adamson144144@gmail.com>
2021-08-07 12:49:49 +0200

    URLs fix

--- printer2/urls.py
+++ printer2/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from printer2 import views
 
 urlpatterns = [
-	path('printer2', views.render_printer2),
+	path('', views.render_printer2),
 ]
--- printer3/urls.py
+++ printer3/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from printer3 import views
 
 urlpatterns = [
-	path('printer3', views.render_printer3),
+	path('', views.render_printer3),
 ]
